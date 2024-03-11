b667d873e36f53c4e801f46cd3fcc0fa5dd60e51
leocklaus <leocklaus@gmail.com>
2024-03-11 20:53:55 -0300

    feat: subtask created

---
+++ src/main/java/io/github/leocklaus/projectsmanager/domain/model/SubTask.java
@@ -0,0 +1,35 @@
+package io.github.leocklaus.projectsmanager.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SubTask extends BaseEntity {
+
+    @Column(nullable = false)
+    private String title;
+    private String description;
+    private LocalDateTime due;
+    private boolean isChecked;
+    @ManyToOne
+    @JoinColumn(name = "task_id")
+    private Task task;
+
+    public void setChecked(){
+        this.isChecked = true;
+    }
+    public void setUnchecked(){
+        this.isChecked = false;
+    }
+
+}
