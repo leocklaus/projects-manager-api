56a20ada668d7970a2e10bca121aa160b197c787
leocklaus <leocklaus@gmail.com>
2024-03-11 14:25:13 -0300

    feat: project created

---
+++ src/main/java/io/github/leocklaus/projectsmanager/ProjectsManagerApplication.java
@@ -0,0 +1,13 @@
+package io.github.leocklaus.projectsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/leocklaus/projectsmanager/ProjectsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.leocklaus.projectsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
