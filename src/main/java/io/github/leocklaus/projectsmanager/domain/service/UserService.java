982f13eadaeb63abf0b8480662db45b4f2fb0f6c
leocklaus <leocklaus@gmail.com>
2024-03-11 15:14:12 -0300

    feat: get user by id method created

--- src/main/java/io/github/leocklaus/projectsmanager/domain/service/UserService.java
+++ src/main/java/io/github/leocklaus/projectsmanager/domain/service/UserService.java
@@ -1,7 +1,12 @@
 package io.github.leocklaus.projectsmanager.domain.service;
 
+import io.github.leocklaus.projectsmanager.api.dto.UserOutputDTO;
+import io.github.leocklaus.projectsmanager.domain.exception.UserNotFoundException;
+import io.github.leocklaus.projectsmanager.domain.model.User;
 import io.github.leocklaus.projectsmanager.domain.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class UserService {
@@ -10,5 +15,16 @@ public class UserService {
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public UserOutputDTO findUserById(String id){
+        var user = getUserByIdOrThrowsExceptionIfNotExists(id);
+        return new UserOutputDTO(user);
+    }
+
+    private User getUserByIdOrThrowsExceptionIfNotExists(String id){
+        User user = userRepository.findById(UUID.fromString(id))
+                .orElseThrow(()-> new UserNotFoundException(id));
+        return user;
     }
 }
