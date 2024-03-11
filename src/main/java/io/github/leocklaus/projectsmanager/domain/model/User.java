6d77663f8687beb837bc89334d5f664a85dac560
leocklaus <leocklaus@gmail.com>
2024-03-11 14:46:04 -0300

    fix: users table name

--- src/main/java/io/github/leocklaus/projectsmanager/domain/model/User.java
+++ src/main/java/io/github/leocklaus/projectsmanager/domain/model/User.java
@@ -1,9 +1,6 @@
 package io.github.leocklaus.projectsmanager.domain.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,6 +14,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "users")
 public class User {
 
     @Id
