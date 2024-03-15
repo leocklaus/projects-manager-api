77d60771b3b57524f2d09aeb192bd51b5d46beec
leocklaus <leocklaus@gmail.com>
2024-03-15 17:00:39 -0300

    feat: notification text descriptions created

--- src/main/java/io/github/leocklaus/projectsmanager/domain/model/NotificationType.java
+++ src/main/java/io/github/leocklaus/projectsmanager/domain/model/NotificationType.java
@@ -1,14 +1,23 @@
 package io.github.leocklaus.projectsmanager.domain.model;
 
 public enum NotificationType {
-    NEW_COMMENT,
-    NEW_REPLY,
-    PROJECT_CREATED,
-    ADDED_TO_PROJECT,
-    NEW_TASK_CREATED,
-    NEW_SUBTASK_CREATED,
-    PROJECT_COMPLETED,
-    TASK_COMPLETED,
-    SUBTASK_COMPLETED;
+    NEW_COMMENT("adicionou um novo comentário!"),
+    NEW_REPLY("respondeu ao seu comentário"),
+    PROJECT_CREATED("criou um projeto"),
+    ADDED_TO_PROJECT("adicionou você ao projeto"),
+    NEW_TASK_CREATED("adicionou uma niva tarefa"),
+    NEW_SUBTASK_CREATED("adicionou uma nova subtarefa"),
+    PROJECT_COMPLETED("conclui o projeto"),
+    TASK_COMPLETED("conclui a tarefa"),
+    SUBTASK_COMPLETED("conclui a subtarefa");
 
+    private final String message;
+
+    NotificationType(String message) {
+        this.message = message;
+    }
+
+    public String getMessage(String actorName){
+        return actorName + " " + this.message;
+    }
 }
